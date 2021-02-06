332be0bf51574ef799ec00f882cc0edb220a4ab2
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-02-06 22:01:02 +0300

    added the displayController module for the flow of the game

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const gameBoard = (()=> {
     //can get the array index like this:
     //gameBoard.gameBoardArr()[i];
 })();
+//Module to control the flow of the game
+const displayController = (() => {
+
+    
+})();
 //Factory Function for players
 const players = (name, mark) => {
     const trying = () => console.log("works");
