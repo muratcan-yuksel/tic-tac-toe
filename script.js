5b08929369fa1df5bc0bbdfe6e0a4b77d7d8ac34
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-02-06 21:54:43 +0300

    added a factory function for the players + a console log to try if it's working or not, IT DOES WORK

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+//Game board MODULE
 const gameBoard = (()=> {
     const gameBoardArr = () => [1,2,3,4,"x"];
     return {gameBoardArr};
     //can get the array index like this:
     //gameBoard.gameBoardArr()[i];
 })();
+//Factory Function for players
+const players = (name, mark) => {
+    const trying = () => console.log("works");
+    return {name, mark, trying};
+};
