b4bcf28f8202b0d0c44a4f79b80f7154de254c87
Murat Can Yüksel <muratcanyukselpro@gmail.com>
2021-02-09 23:39:28 +0300

    deleted the unused code

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ const displayController = (() => {
         }
         //invoke the function that let the players take turns
       gameFlow.turnerFunc();
-
+    
     })
     
 })();
@@ -54,93 +54,5 @@ const gameFlow = (() => {
    //return to function to use it in display controller
    return {turnerFunc};
 })();
-
-
-
-/*
-
-//display controller module
-const displayController = (() => {
-    //create a reusable No operation function
-//this function does nothing
-let noop = () => {};
-//create the function that'll take the unnecessary X from the array
-let foo = () => {
-    //swap the functions
-    foo=noop;
-    gameBoard.boardArr.pop();
-   console.log("yoyo");
-}
-    //take the last item of the boardArr array and display it on the clicked square
-    squares.addEventListener("click", function (e){  
-        if (e.target.className ==="squares"){
-                e.target.textContent= gameBoard.boardArr[gameBoard.boardArr.length -1];
-                foo();     
-        }
-        //invoke the function that let the players take turns
-      gameFlow.turnerFunc();
-
-    })
-})();
-//variable to check the condition of turning rounds
-let turn = true;
-//Module to control the flow of the game
-const gameFlow = (() => {
-    //function that let the players take turns
-   const turnerFunc = () => {
-    if(turn){
-        playerOne.play();
-    }else {
-        playerTwo.play();
-    }
-    turn=!turn;
-   }
-
-   
-   //return to function to use it in display controller
-   return {turnerFunc};
-})();
-
-*/
-
-/*
-//create a reusable No operation function
-//this function does nothing
-let noop = () => {};
-//create the function that'll take the unnecessary X from the array
-let foo = () => {
-    //swap the functions
-    foo=noop;
-    gameBoard.boardArr.pop();
-}
-*/
-
-/*
- //if the array is empty, mark X (the first player)
-            if (gameBoard.boardArr[gameBoard.boardArr.length -1] === undefined){
-                e.target.textContent= "X";
-            }
-            */
-
-
-/*
- //make sure the first mark is X
-    if (typeof )
-    const firstMark = (function() {
-        let executed = false;
-        return function() {
-            if (!executed) {
-                executed = true;
-                // do something
-            }
-        };
-    })();
-
-*/
-
-
-
-
-
 
 
